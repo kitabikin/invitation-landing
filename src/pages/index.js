5f7b6eb556fff894febcb7b02026039196fcabd5
rendiriz <rendirizkir@gmail.com>
2021-12-27 21:43:36 +0700

    feat: google analytic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
+import Script from 'next/script'
 import ContainerBlank from '@/layouts/container/containerBlank'
 import { NextSeo } from 'next-seo'
 import site from '@/config/site'
 
 const isProduction = process.env.ENVIRONMENT === 'production'
+const gaID = process.env.NEXT_PUBLIC_GA_ID
 
 import { Container } from '@chakra-ui/react'
 import ComingSoon from '@/components/specific/comingSoon'
@@ -13,6 +15,24 @@ function Home() {
 
   return (
     <>
+      {isProduction && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${gaID}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', '${gaID}');
+            `}
+          </Script>
+        </>
+      )}
+
       <NextSeo
         title={site.title}
         description={site.description}
