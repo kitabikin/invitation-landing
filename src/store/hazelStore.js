c853732b79ddb68cc7593ba6ffeacee97f407088
Rendi Rizki <rendirizkir@gmail.com>
2023-06-03 18:31:04 +0700

    fix: music

--- src/store/hazelStore.js
+++ src/store/hazelStore.js
@@ -3,4 +3,4 @@ import { atom } from 'jotai';
 export const themeAtom = atom('theme-red');
 export const displayAtom = atom('block');
 export const overflowYAtom = atom('hidden');
-export const isPlayingAtom = atom(false);
+export const isPlayingAtom = atom(true);
