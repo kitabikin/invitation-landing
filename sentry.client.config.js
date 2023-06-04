b059d9484d695889e13386eff91d93ae1d7451ab
Rendi Rizki <rendirizkir@gmail.com>
2023-06-05 00:37:54 +0700

    chore: sentry session replay

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -6,11 +6,13 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   environment: ENVIRONMENT,
   dsn: SENTRY_DSN,
-  tracesSampler: () => {
-    if (ENVIRONMENT === 'production') {
-      return 0.1;
-    }
-
-    return 0.5;
-  },
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  integrations: [
+    new Sentry.Replay({
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
 });
--- sentry.server.config.js
+++ sentry.server.config.js
@@ -6,11 +6,5 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   environment: ENVIRONMENT,
   dsn: SENTRY_DSN,
-  tracesSampler: () => {
-    if (ENVIRONMENT === 'production') {
-      return 0.1;
-    }
-
-    return 0.5;
-  },
+  tracesSampleRate: 1.0,
 });
